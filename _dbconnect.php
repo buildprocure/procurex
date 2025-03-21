135383a9dcfc09eb6239311d01805ab09b80847f
unknown <rabin.khadka0@gmail.com>
2025-03-21 15:53:43 -0400

    Added evn files setup, load env files based on the environment we were deploying

--- _dbconnect.php
+++ _dbconnect.php
@@ -1,17 +1,21 @@
 <?php
 ob_start();
+include 'config.php';
 /* $update = false;
 $delete = false; */
   // Connect to the database
-  $servername = "html-db-1";
-  $username = "root";
-  $password = "@Shova595Bhandari";
-  $database = "ilife";
+// $host = '143.198.64.132';
+// $db   = 'ilife';
+// $user = 'root';
+// $pass = '@Shova595Bhandari';
+// $port = '3306';
 
-  //Create a connection
-  $conn = mysqli_connect($servername, $username, $password, $database);
+// Use mysqli to connect to MySQL
+
+  $conn = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_PORT);
+
   // Die if connection was not successful
-if (!$conn){
+ if (!$conn){
     die("Sorry we failed to connect to database: ". mysqli_connect_error());
-}
+ }
 ?>
