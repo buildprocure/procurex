9ebd3e4a1e6c6604a6d672b7fa5dde7b94631a8c
Shova <shovabhndr@gmail.com>
2025-04-18 23:58:52 -0400

    8: Added header.php to load horizontal nav and vertical nav from single file

--- Admin/_loggedindatabase.php
+++ Admin/_loggedindatabase.php
@@ -13,7 +13,7 @@
   $noteIDerror = false;
   
   include '../_dbconnect.php';
-  session_start();
+  
 
   if(!isset($_SESSION['loggedin'])|| $_SESSION['loggedin'] != true){
       header("location: index.php");
