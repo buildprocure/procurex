8e60496812fc80cca09b27ad69fb731779aa4923
unknown <rabin.khadka0@gmail.com>
2025-06-03 14:40:33 -0400

    Updated logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-
-# Only run daemon if explicitly needed
-if [ "$SITE24X7_ENABLE" = "true" ] && [ -x /opt/site24x7/S247DataExporter/bin/S247DataExporterDaemon ]; then
-  /opt/site24x7/S247DataExporter/bin/S247DataExporterDaemon start
-fi
-
-# Start Apache
-apache2-foreground
+#Start S247DataExporter
+sh /opt/S247DataExporter/bin/service.sh start
+#
+#Your code
+#
+php-fpm
