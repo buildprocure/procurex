6499985bc424feca97ed96b7bd0bbbec1dc5a366
unknown <rabin.khadka0@gmail.com>
2025-06-19 21:51:29 -0400

    Updated azure-pipeline.yml and docker-compose.yml to integrate simplesamlphp from separate repo

--- login.php
+++ login.php
@@ -174,7 +174,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         <br><br><br><br><br><br>
 
 
-        <a style="text-decoration: none;" href="./signup.php">New Here? Sign up </a>
+        <a style="text-decoration: none;" href="./Sign_up.php">New Here? Sign up </a>
         
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
