392a8d3c25c9e2a63b577e24e02b239e53755c00
unknown <rabin.khadka0@gmail.com>
2025-10-14 10:09:45 -0400

    Added sudo on entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #Start S247DataExporter
 echo "Starting S247DataExporter..."
-sh /opt/S247DataExporter/bin/service.sh start
+sudo sh /opt/S247DataExporter/bin/service.sh start
 #
 #Your code
 #
