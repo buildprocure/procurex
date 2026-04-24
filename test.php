6b0ff3fc8cdb07abe90f80c3139d2f0175ce1dfa
Shova <shovabhndr@gmail.com>
2026-04-23 22:17:16 -0400

    Removed script from test file for testing purpose

--- test.php
+++ test.php
@@ -954,16 +954,7 @@ font-weight: 12px !important;
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "none") {
-      content.style.display = "block";
-    } else {
-      content.style.display = "none";
-    }
-  });
+f
 }
 </script>
 		<?php
